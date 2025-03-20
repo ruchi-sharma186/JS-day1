626aa6a13a1e2ea93a3aef26bd22336f4f447ffe
ruchi-sharma186 <ruchisharmasharma216@gmail.com>
2025-03-20 21:17:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+var a =15;
+var a =55;
+console.log(a);
+
+
+let b = 520;
+  b=80
+console.log(b);
+
+
+const c=50
+//c=180
+console.log(c)
+
+num = 40;
+console.log(num);
+   
+
+
